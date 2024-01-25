d0c0f116f0945c1328a12d1447e68f7068353d12
lionpyw <lionpaaji@gmail.com>
2024-01-25 15:14:16 +0500

    modified views using drf

--- auctions/views.py
+++ auctions/views.py
@@ -4,39 +4,41 @@ from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import FormView
+from rest_framework import status, permissions
+from rest_framework.decorators import permission_classes
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .forms import CreateAuctionForm, CommentFieldForm
 from .models import AuctionListings,Category, Comments,Bid
 
 User = get_user_model()
 
-def index(request):
-    listing = AuctionListings.objects.filter(is_active=True).all()
-    if request.user.is_authenticated:
-        expired = AuctionListings.objects.filter(is_active=False, bidwinner=request.user)
-        bought = False
-        if expired.count() > 0:
-            bought = True
+class AuctionListing(APIView):
+
+    def get(self, request):
+        listing = AuctionListings.objects.filter(is_active=True).all()
+        if self.request.user.is_authenticated:
+            expired = AuctionListings.objects.filter(is_active=False, bidwinner= self.request.user)
+            bought = False
+            if expired.count() > 0:
+                bought = True
+            return render(request, "auctions/index.html", {
+                'items' : listing,
+                'buys' : expired,
+                'bought' : bought
+            })
         return render(request, "auctions/index.html", {
-            'items' : listing,
-            'buys' : expired,
-            'bought' : bought
-        })
-    return render(request, "auctions/index.html", {
-            'items' : listing
-        })
+                'items' : listing
+            })
 
-
-@login_required
-def create_listing(request):
-    form = CreateAuctionForm()
-    if request.method == 'GET':
-        return render(request, "auctions/create.html",{
-            "form": form
-        })
-    if request.method == 'POST':
+    @permission_classes([permissions.IsAuthenticated])
+    def post(self,request):
         form = CreateAuctionForm(request.POST)
         if form.is_valid():
-            form.instance.user = request.user
+            form.instance.user = self.request.user
             form.save()
             return HttpResponseRedirect(reverse("index"))
         else:
@@ -45,93 +47,103 @@ def create_listing(request):
                 "form": form
             })
 
+class AuctionFormView(FormView):
+    template_name = "auctions/create.html"
+    form_class = CreateAuctionForm
+    success_url = "/index/"  
+            
+class AuctionDetail(APIView):
+    def get(self,request,pk):
+        listing = AuctionListings.objects.get(pk=pk)
+        watch = self.request.user in listing.watchlist.all()
+        form = CommentFieldForm()
+        comment = Comments.objects.filter(listing=listing)
+        return render(request, "auctions/listing.html", {
+            'item' : listing,
+            'watch' : watch,
+            'comments' : comment,
+            'form' : form
+        })
     
-def listing(request,id):
-    listing = AuctionListings.objects.get(pk=id)
-    watch = request.user in listing.watchlist.all()
-    form = CommentFieldForm()
-    comment = Comments.objects.filter(listing=listing)
-    return render(request, "auctions/listing.html", {
-        'item' : listing,
-        'watch' : watch,
-        'comments' : comment,
-        'form' : form
+    @permission_classes([permissions.IsAuthenticated])
+    def post(self, request, pk):
+        listing=AuctionListings.objects.get(pk=pk)
+        if listing.user == request.user:
+            listing.is_active = False
+            listing.watchlist.clear()
+            listing.save()
+        return HttpResponseRedirect(reverse('listing', args=(pk, )))
+    
+class CategoryList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'auctions/categories.html'
+
+    def get(self, request):
+        all_category = Category.objects.all()
+        listings = AuctionListings.objects.filter()
+        return Response({
+        'items' : listings,
+        'category' : all_category
     })
 
+class CategoryDetail(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "auctions/index.html"
+    def get(self,request, title):
+        category= Category.objects.get(title=title)
+        listings = AuctionListings.objects.filter(category=category, is_active= True)
+        return Response({
+            'items' : listings
+        })
+    
+class WatchListList(APIView):
+    def get(self,request):
+        if self.request.user.is_authenticated:
+            listing = self.request.user.watchlist.all()
+        return render(request, "auctions/index.html", {
+            'items' : listing
+        })
 
-def categories(request):
-    all_category = Category.objects.all()
-    listings = AuctionListings.objects.filter()
-    return render(request, "auctions/categories.html", {
-    'items' : listings,
-    'category' : all_category
-})
+class WatchListDetail(APIView):
+    permission_classes=[permissions.IsAuthenticated]
 
-def category(request, title):
-    category= Category.objects.get(title=title)
-    listings = AuctionListings.objects.filter(category=category, is_active= True)
-    return render(request, "auctions/index.html", {
-        'items' : listings
-    })
-
-
-@login_required
-def comment(request, id):   
-    listing = AuctionListings.objects.get(pk=id)
-    if request.method == 'POST':
-        form = CommentFieldForm(request.POST)
-        if form.is_valid():
-            form.instance.user = request.user
-            form.instance.listing = listing
-            form.save()
-            return HttpResponseRedirect(reverse("listing",args=(id, )))
-
-
-def watchlist(request):
-    if request.user.is_authenticated:
-        listing = request.user.watchlist.all()
-    return render(request, "auctions/index.html", {
-        'items' : listing
-    })
+    def get(self,request,pk):
+        listing = AuctionListings.objects.get(pk=pk)
+        listing.watchlist.add(self.request.user)
+        return HttpResponseRedirect(reverse("listing", args=(pk,)))
+    
+    def post(self, request, pk):
+        listing = AuctionListings.objects.get(pk=pk)
+        listing.watchlist.remove(self.request.user)
+        return HttpResponseRedirect(reverse("listing",args=(pk,)))
 
 
-@login_required
-def addwatchlist(request, id):
-    listing = AuctionListings.objects.get(pk=id)
-    listing.watchlist.add(request.user)
-    return HttpResponseRedirect(reverse("listing",args=(id, )))
-
-
-@login_required
-def remove_watchlist(request, id):
-    listing = AuctionListings.objects.get(pk=id)
-    listing.watchlist.remove(request.user)
-    return HttpResponseRedirect(reverse("listing",args=(id, )))
-
-
-@login_required
-def bid(request, id):
-    listing=AuctionListings.objects.get(pk=id)
-    bidValue=float(request.POST['bid'])
-    (bid, created) = Bid.objects.get_or_create(listing=listing, user=request.user, placed_bid=bidValue)
-    if bidValue > listing.price and request.user != listing.user:
-        bid.user=request.user
-        bid.placed_bid=bidValue
-        bid.save()
-        listing.price = bidValue
-        listing.bidwinner = request.user
-        listing.save()
-    return HttpResponseRedirect(reverse('listing', args=(id, )))
-
-
-@login_required
-def endAuction(request, id):
-    listing=AuctionListings.objects.get(pk=id)
-    if request.user.is_authenticated and listing.user == request.user:
-        listing.is_active = False
-        listing.watchlist.clear()
-        listing.save()
-    return HttpResponseRedirect(reverse('listing', args=(id, )))
+class Comment(APIView):   
+    permission_classes=[permissions.IsAuthenticated]
+    def post(self,request, pk):   
+        listing = AuctionListings.objects.get(pk=pk)
+        if request.method == 'POST':
+            form = CommentFieldForm(request.POST)
+            if form.is_valid():
+                form.instance.user = self.request.user
+                form.instance.listing = listing
+                form.save()
+                return HttpResponseRedirect(reverse("listing",args=(pk, )))
+            
+class BidView(APIView):
+    @permission_classes([permissions.IsAuthenticated])
+    def post(self, request, pk):
+        listing=AuctionListings.objects.get(pk=pk)
+        bidValue=float(self.request.POST['bid'])
+        (bid, _) = Bid.objects.get_or_create(listing=listing, user=self.request.user, placed_bid=bidValue)
+        if bidValue > listing.price and self.request.user != listing.user:
+            bid.user = self.request.user
+            bid.placed_bid = bidValue
+            bid.save()
+            listing.price = bidValue
+            listing.bidwinner = self.request.user
+            listing.save()
+        return HttpResponseRedirect(reverse('listing', args=(pk, )))
 
 
 def login_view(request):
